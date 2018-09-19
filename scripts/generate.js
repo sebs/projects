6ebd388d6836945afb5b85685f9eb8c0fb5876fa
Sebastian Schürmann <sebs@2xs.org>
2018-09-19 16:39:02 +0200

    Readme generator

---
+++ scripts/generate.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+var projects = require('../projects.json');
+var github = require('octonode');
+var client = github.client();
+projects = projects.map(project => fetch(`https://raw.githubusercontent.com/${project}/master/package.json`).then(res => res.json()));
+Promise
+	.all(projects)
+	.then((packages) =>{
+		return packages.map(package => `
+# ${package.name}
+
+${package.description}
+
+[Project Page](${package.homepage})
+
+`)
+	}).then(console.log)
