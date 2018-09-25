b5801b4fcf1356e236589e0fe79ef87e5fd75033
Sebastian Schürmann <sebs@2xs.org>
2018-09-25 17:21:44 +0200

    Download count year

--- scripts/generate-stats.js
+++ scripts/generate-stats.js
@@ -1,3 +1,27 @@
 const fetch = require('node-fetch');
 var projects = require('../projects.json');
-projects = projects.map(project => fetch(`https://raw.githubusercontent.com/${project}/master/package.json`).then(res => res.json()));
+
+
+function fetchProject(project) {
+	return fetch(`https://raw.githubusercontent.com/${project}/master/package.json`)
+		.then(res => res.json())
+}
+
+function fetchStats(projectName, range) {
+	return fetch(`https://api.npmjs.org/downloads/point/last-year/${projectName}`)
+		.then(res => res.json())
+}
+
+const add = (a, b) => a + b
+
+projects = projects.map(project => fetchProject(project));
+Promise
+	.all(projects) // all projects
+	.then(projects=>projects.map(project=>project.name)) // extract the names
+	.then(projectNames=>projectNames.map(name=>fetchStats(name))) // get stats for names
+	.then(res=>Promise.all(res)) // resolve results
+	.then(stats=>stats.map(datum=>datum.downloads)) // only downloads
+	.then(stats=>stats.filter(datum=>typeof datum !== 'undefined')) // only download
+	.then(downloadCounts=>downloadCounts.reduce(add))
+	.then(console.log)
+// https://api.npmjs.org/downloads/point/last-week/express/
