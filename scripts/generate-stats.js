00e2b0b0939d948a63140acecc5570b5a4260dc2
Sebastian Schürmann <sebs@2xs.org>
2018-09-26 09:56:37 +0200

    Add stats generation

--- scripts/generate-stats.js
+++ scripts/generate-stats.js
@@ -4,12 +4,12 @@ var projects = require('../projects.json');
 
 function fetchProject(project) {
 	return fetch(`https://raw.githubusercontent.com/${project}/master/package.json`)
-		.then(res => res.json())
+		.then(res => res.json()).catch(console.error)
 }
 
 function fetchStats(projectName, range) {
 	return fetch(`https://api.npmjs.org/downloads/point/last-year/${projectName}`)
-		.then(res => res.json())
+		.then(res => res.json()).catch(console.error)
 }
 
 const add = (a, b) => a + b
@@ -23,4 +23,6 @@ Promise
 	.then(stats=>stats.map(datum=>datum.downloads)) // only downloads
 	.then(stats=>stats.filter(datum=>typeof datum !== 'undefined')) // only downloads with counts
 	.then(downloadCounts=>downloadCounts.reduce(add))
-	.then(result=>console.log)
+	.then(count=>console.log({count}))
+	.catch(console.error)
+// https://api.npmjs.org/downloads/point/last-week/express/
